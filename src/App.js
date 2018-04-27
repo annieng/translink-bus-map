a1c8e82be063c672c3bcb4dc98fb251f2ad53ba5
Annie Ng <annieng1991@gmail.com>
2018-04-27 16:40:14 -0700

    more styling

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <img className='logo' src='./images/bus.svg' alt='bus-icon' />
         <div className='header'>
-         <h1 className='title'> Translink Bus Mapper </h1>
+          <h1 className='title'> Translink Bus Mapper </h1>
+        </div>
+        <div className='instructions'>
+          <p> CLICK ON A </p>
+          <p> DOT TO </p>
+          <p> DISPLAY BUS </p>
+          <p> INFORMATION </p>
         </div>
         <div className='map-container'>
           <Map />
