5ab030f81c501da9875948f238f59f60ec3a5a81
Annie Ng <annieng1991@gmail.com>
2018-05-02 17:18:10 -0700

    code clean up

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,19 +1,7 @@
 import * as types from './actionTypes'
-// var host = process.env.HOST || '0.0.0.0';
-// var port = process.env.PORT || 8080;
-
-// var cors_proxy = require('cors-anywhere');
-
 
 const request = require('superagent')
 
-// cors_proxy.createServer({
-//   originWhitelist: [], // Allow all origins
-//   requireHeader: ['origin', 'x-requested-with'],
-//   removeHeaders: ['cookie', 'cookie2']
-// }).listen(port, host, function () {
-//   console.log('Running CORS Anywhere on ' + host + ':' + port);
-// });
 
 export function receiveBusses(json) {
   return {type: types.RECEIVE_BUSSES, busses: json}
@@ -21,7 +9,7 @@ export function receiveBusses(json) {
 
 export function fetchBusses() {
   return dispatch => {
-    //const url = 'http://api.translink.ca/rttiapi/v1/buses?apikey=d3oX05CBjgbrmmeUoAzm'
+    // fixing cors error 
     const url = 'http://api.translink.ca/rttiapi/v1/buses?apikey=d3oX05CBjgbrmmeUoAzm'
     const proxyurl = 'cors-anywhere.herokuapp.com/'
     return request
@@ -29,11 +17,10 @@ export function fetchBusses() {
     .query('d3oX05CBjgbrmmeUoAzm')
     .set('Accept', 'application/json')
     .buffer(true)
-    //.set("Access-Control-Allow-Origin", "*")
-    //.withCredentials()
     .then(res => res.body)
     .then(json => dispatch(receiveBusses(json)))
     .catch(err => console.log(err))
   }
 }
 
+ 
